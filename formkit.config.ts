2d5bacf34e4e42aaca3ad1188211973f8255f181
Paul Morrison <paul.unr@gmail.com>
2024-11-04 11:28:20 -0500

    THE FIX

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,8 +1,11 @@
 import { defineFormKitConfig } from '@formkit/vue'
 import { rootClasses } from './formkit.theme'
+import { genesisIcons } from "@formkit/icons"
+
 
 export default defineFormKitConfig({
 	config: {
 		rootClasses,
 	},
+	icons: genesisIcons
 })
